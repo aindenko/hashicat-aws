6c70a2400b71a7745422d9abbe95ea9c797fe1b3
Andrii Indenko <rijndaelmail@gmail.com>
2020-11-19 11:33:30 +0000

    Added bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------------
 // Variables
-variable "s3_bucket_acceleration_status" = "Enabled"
-variable "s3_bucket_bucket" = "aindenko"
+# variable "s3_bucket_acceleration_status" = "Enabled"
+#variable "s3_bucket_bucket" = "aindenko"
 variable "s3_bucket_request_payer" {}
 
 //--------------------------------------------------------------------
@@ -10,8 +10,8 @@ module "s3_bucket" {
   source  = "app.terraform.io/aindenko/s3-bucket/aws"
   version = "1.15.0"
 
-  acceleration_status = "${var.s3_bucket_acceleration_status}"
-  bucket = "${var.s3_bucket_bucket}"
+  acceleration_status = "Enabled"
+  bucket = "aindenko"
   bucket_prefix = "aindenko"
  # policy = "${var.s3_bucket_policy}"
 #  request_payer = "${var.s3_bucket_request_payer}"
